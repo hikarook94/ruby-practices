950c51a6d8ff02f4abe19e661570922b7ab5e927
uhikarudevjapan <walrus4l145@icloud.com>
2021-11-12 23:27:07 +0900

    カレンダーコマンド実装

---
+++ 02.calendar/my-calender.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+require "date"
+require 'optparse'
+
+default_date = Date.today
+option_year = default_date.year
+option_month = default_date.month
+
+options = ARGV.getopts("y:m:")
+
+# p options["y"]
+# p options["m"]
+
+#-y-mそれぞれ引数を取った場合(nilじゃなかった場合)
+option_year = options["y"].to_i unless options["y"] == nil
+option_month = options["m"].to_i unless options["m"] == nil
+
+# p(option_year, option_month)
+
+start_date = Date.new(option_year, option_month)
+last_date = Date.new(option_year, option_month, -1)
+week_labels = ["日", "月", "火", "水", "木", "金", "土"]
+
+# 中央揃えにするために半角スペースを6つ頭につけている
+puts("      #{start_date.month}月 #{start_date.year}")
+puts week_labels.join(" ")
+
+# 初日の曜日を揃えるためにスペースを最初に表示しておく
+start_date.wday.times {
+  print("  ", " ")
+}
+start_date.step(last_date) {|date|
+  day_string = date.day.to_s
+  if day_string.size == 1
+    day_string = " " + day_string
+  end
+
+  if date == default_date
+    day_string = "\e[7m#{day_string}\e[0m"
+  end
+
+  if date.saturday? || date == last_date
+    day_string += "\n"
+  else
+    day_string += " "
+  end
+  print day_string
+}
+puts "\n"
