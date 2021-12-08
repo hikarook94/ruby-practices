89bf6fcf8402e9fbbbb8b99be940dfb42276979d
uhikarudevjapan <walrus4l145@icloud.com>
2021-12-08 19:59:07 +0900

    最終フレームの分岐削除、スペアの条件分岐のネストを1階層浅くする

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -14,30 +14,23 @@ def calc_score(score)
   end
 end
 
-frames.times do |frame|
-  case frame
-  # 最終フレーム目
-  when 9
-    # 最終フレームの時点で残っている投球数の数だけループを回してscoreに加算していく
-    (scores.size - delivered_count).times do |t|
-      score += calc_score(scores.reverse[t])
-    end
-  # 1~9フレーム目
+frames.times do
+  # ストライクの場合
+  if scores[delivered_count] == 'X'
+    score += 10
+    score += calc_score(scores[delivered_count + 1])
+    score += calc_score(scores[delivered_count + 2])
+    delivered_count += 1
+  # スペアの場合
+  elsif scores[delivered_count].to_i + scores[delivered_count + 1].to_i == 10
+    score += scores[delivered_count].to_i
+    score += calc_score(scores[delivered_count + 1])
+    score += calc_score(scores[delivered_count + 2])
+    delivered_count += 2
   else
-    # ストライクの場合
-    if scores[delivered_count] == 'X'
-      score += 10
-      score += calc_score(scores[delivered_count + 1])
-      score += calc_score(scores[delivered_count + 2])
-      delivered_count += 1
-    else
-      score += scores[delivered_count].to_i
-      score += calc_score(scores[delivered_count + 1])
-      frame_total_point = scores[delivered_count].to_i + scores[delivered_count + 1].to_i
-      # スペアの場合
-      score += calc_score(scores[delivered_count + 2]) if frame_total_point == 10
-      delivered_count += 2
-    end
+    score += scores[delivered_count].to_i
+    score += calc_score(scores[delivered_count + 1])
+    delivered_count += 2
   end
 end
 puts score
