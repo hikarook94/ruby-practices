235cc9c81d6ca56877107ffe62387022f2cc26f7
uhikarudevjapan <walrus4l145@icloud.com>
2022-06-25 22:58:11 +0900

    ボウリングプログラムを追加

---
+++ 07.bowling_object/game.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require_relative 'frame'
+
+class Game
+  attr_reader :frames
+
+  def initialize(marks)
+    pins = parse_marks(marks)
+    @frames = generate_frames(pins)
+  end
+
+  def score
+    @frames.map.sum(&:total_score)
+  end
+
+  private
+
+  def parse_marks(marks)
+    marks.split(',').map { |m| m == 'X' ? 10 : m.to_i }
+  end
+
+  def generate_frames(pins)
+    frames = []
+    9.times do
+      rolls = pins.shift(2)
+      if rolls.first == 10
+        frame = Frame.new(rolls.first)
+        frames << frame
+        pins.unshift(rolls.last)
+        frame.bonus = pins[0, 2]
+      else
+        frame = Frame.new(*rolls)
+        frame.bonus = pins[0, 1] if frame.spare?
+        frames << frame
+      end
+    end
+    frames << Frame.new(*pins)
+  end
+end
